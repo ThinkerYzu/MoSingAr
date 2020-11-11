070df6bca2f7edb80969c2639c3b9037aa5e9e53
Thinker Lee <thinker@codemud.net>
2020-11-11 12:10:51 -0800

    Muxer creates channels over a transport.

---
+++ mux/mux.cpp
@@ -0,0 +1,566 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ */
+#include "mux.h"
+
+#include <memory>
+#include <string.h>
+
+#define min(x, y) ((x) <= (y) ? (x) : (y))
+#define max(x, y) ((x) >= (y) ? (x) : (y))
+
+void
+Channel::Close() {
+  mMux->ChannelAskClose(mChanId);
+}
+
+bool
+Channel::Send(MuxPacket* aPacket) {
+  if (!IsValid()) {
+    return false;
+  }
+  assert(aPacket->mChan == mChanId);
+  aPacket->mSeq = mSeq++;
+  if (aPacket->mSeq < first_seq) {
+    aPacket->mSeq = first_seq;
+  }
+  auto ok = mMux->DoSend(aPacket);
+  if (!ok) {
+    mValid = false;
+  }
+  return ok;
+}
+
+void
+Channel::HandleIncomingPacket(const MuxPacket* aPacket) {
+  assert(aPacket->mChan == mChanId);
+  assert(aPacket->mSeq == mSeqPeer);
+  assert(IsValid());
+
+  assert(aPacket->mCmd != MuxPacket::MuxCmd::Data ||
+         (reinterpret_cast<const DataPacket*>(aPacket)->mPayloadSize + sizeof(DataPacket)
+          == aPacket->mSize));
+
+  mSeqPeer++;
+  mSeqPeer = max(mSeqPeer, first_seq);
+  if (mListener) {
+    mListener->OnReceive(this, aPacket);
+  }
+}
+
+bool
+StreamChannel::Write(unsigned int aSize, const char *aData) {
+  auto remain = aSize;
+  auto ptr = aData;
+  while (remain > 0) {
+    auto tocopy = min(remain, DataPacket::MaxPayloadSize);
+    std::unique_ptr<DataPacket> pkt(DataPacket::Create(tocopy, GetChannelId()));
+    memcpy(pkt->mPayload, ptr, tocopy);
+    auto ok = Send(pkt.get());
+    if (!ok) {
+      return false;
+    }
+    ptr += tocopy;
+    remain -= tocopy;
+  }
+  return true;
+}
+
+void
+StreamChannel::OnReceive(Channel* aChannel, const MuxPacket* aPacket) {
+  assert(aPacket->mCmd == MuxPacket::MuxCmd::Data);
+  if (mListener) {
+    auto datapkt = reinterpret_cast<const DataPacket*>(aPacket);
+    mListener->OnReceive(this, datapkt->mPayloadSize, datapkt->mPayload);
+  }
+}
+
+void
+StreamChannel::OnClose(Channel* aChannel) {
+  assert(mListener != nullptr);
+  mListener->OnClose(this);
+}
+
+void
+StreamChannel::OnError(Channel* aChannel, Channel::Error aErr) {
+  assert(mListener != nullptr);
+  mListener->OnError(this, aErr);
+}
+
+Mux::Mux(MuxSide aSide)
+  : mSide(aSide)
+  , mControlChan(0, 0, this)
+  , mNextChanId(4 + (0x1 & (int)aSide)) {
+  mControlChan.SetListener(this);
+}
+
+unsigned int
+Mux::Open(const char* aPath) {
+  std::unique_ptr<OpenChanPacket> open(OpenChanPacket::Create(aPath));
+  auto ok = mControlChan.Send(open.get());
+  if (!ok) {
+    return 0;
+  }
+  auto seq = open->mSeq;        // filled by |Channel::Send()|
+  mWaitingPackets[seq] = open.release();
+  return seq;
+}
+
+void
+Mux::OnReceive(Channel* aChannel, const MuxPacket* aPacket) {
+  assert(aChannel == &mControlChan);
+  switch(aPacket->mCmd) {
+  case MuxPacket::MuxCmd::Hello:
+    assert(aPacket->mSize == sizeof(MuxPacket));
+    break;
+
+  case MuxPacket::MuxCmd::OpenChan:
+    {
+      assert(mChanListener != nullptr);
+      auto request = reinterpret_cast<const OpenChanPacket*>(aPacket);
+      assert(OpenChanPacket::SizeOfPathLen(request->mPathLen) == aPacket->mSize);
+      assert(request->mPath[request->mPathLen] == 0);
+      Channel *channel;
+      auto sz = mChanListener->OnCheckDataSize(request->mPath);
+      if (sz < 0) {
+        auto reply =
+          std::make_unique<OpenChanReplyPacket>(request->mSeq,
+                                                OpenChanReplyPacket::UnknownError,
+                                                0,
+                                                0);
+        mControlChan.Send(reply.get());
+        break;
+      }
+      if (sz == 0) {
+        auto channel_ = new StreamChannel(mNextChanId, this);
+        channel = channel_->GetChannel();
+      } else {
+        channel = new Channel(mNextChanId, sz, this);
+      }
+      auto chan_id = mNextChanId;
+      mChannels[chan_id] = channel;
+      mNextChanId += 2;
+      while (GetChannel(mNextChanId)) {
+        mNextChanId += 2;
+      }
+
+      auto reply =
+        std::make_unique<OpenChanReplyPacket>(request->mSeq,
+                                              OpenChanReplyPacket::Ok,
+                                              sz,
+                                              chan_id);
+      mControlChan.Send(reply.get());
+      mChanListener->OnChannelOpened(request->mSeq, 0, channel, request->mPath, false);
+      break;
+    }
+
+  case MuxPacket::MuxCmd::OpenChanReply:
+    {
+      assert(mChanListener != nullptr);
+      assert(aPacket->mSize == sizeof(OpenChanReplyPacket));
+
+      auto reply = reinterpret_cast<const OpenChanReplyPacket*>(aPacket);
+      assert(GetChannel(reply->mDataChan) == nullptr);
+      assert((reply->mDataChan & CHANNEL_MASK) != (int)mSide);
+
+      auto pitr = mWaitingPackets.find(reply->mReqSeq);
+      assert(pitr != mWaitingPackets.end());
+
+      std::unique_ptr<OpenChanPacket> request(reinterpret_cast<OpenChanPacket*>(pitr->second));
+      assert(request->mCmd == MuxPacket::MuxCmd::OpenChan);
+
+      mWaitingPackets.erase(pitr);
+
+      if (reply->mErrno != 0) {
+        mChanListener->OnChannelOpened(reply->mReqSeq, reply->mErrno, nullptr, nullptr, true);
+        break;
+      }
+
+      Channel* channel;
+      if (reply->mDataSize == 0) {
+        auto channel_ = new StreamChannel(reply->mDataChan, this);
+        channel = channel_->GetChannel();
+      } else {
+        channel = new Channel(reply->mDataChan, reply->mDataSize, this);
+      }
+      mChannels[reply->mDataChan] = channel;
+      mChanListener->OnChannelOpened(reply->mReqSeq, 0, channel, request->mPath, true);
+      break;
+    }
+  }
+}
+
+void
+Mux::HandleIncomingPacket(const MuxPacket* aPacket) {
+  switch (aPacket->mCmd) {
+  case MuxPacket::MuxCmd::CloseChan:
+    {
+      // It can fail silently since both sides can send CloseChan
+      // packets much the same time.
+      assert(aPacket->mSize == sizeof(MuxPacket));
+      DoCloseChannel(aPacket->mChan);
+      break;
+    }
+
+  case MuxPacket::MuxCmd::Nak:
+    {
+      assert(aPacket->mSize == sizeof(MuxPacket));
+      DoErrorChannel(aPacket->mChan, Channel::Error::UnknownError);
+      break;
+    }
+
+  default:
+    {
+      // Route to the associated channel.
+      auto channel = GetChannel(aPacket->mChan);
+      if (channel == nullptr) {
+        SendNak(aPacket->mChan, aPacket->mSeq);
+        break;
+      }
+      if (channel->IsValid()) {
+        channel->HandleIncomingPacket(aPacket);
+      } else {
+        SendNak(aPacket->mChan, aPacket->mSeq);
+      }
+      break;
+    }
+  }
+}
+
+void
+Mux::OnClose(Channel* aChannel) {
+}
+
+void
+Mux::OnError(Channel* aChannel, PacketListener::Error aErr) {
+}
+
+void
+Mux::ChannelAskClose(unsigned int aChanId) {
+  auto channel = GetChannel(aChanId);
+  assert(channel != nullptr);
+
+  if (channel->IsValid()) {
+    auto close = std::make_unique<MuxPacket>(sizeof(MuxPacket), aChanId, MuxPacket::MuxCmd::CloseChan);
+    channel->Send(close.get());
+  }
+  
+  auto ok = DoCloseChannel(aChanId);
+  assert(ok);
+}
+
+bool
+Mux::DoCloseChannel(unsigned int aChanId) {
+  auto chan = GetChannel(aChanId);
+  if (chan == nullptr) {
+    return false;
+  }
+  chan->OnClose();
+  mChannels.erase(aChanId);
+  delete chan;
+  return true;
+}
+
+bool
+Mux::DoErrorChannel(unsigned int aChanId, Channel::Error aErr) {
+  auto chan = GetChannel(aChanId);
+  if (chan == nullptr) {
+    return false;
+  }
+  chan->OnError((Channel::Error)aErr);
+  mChannels.erase(aChanId);
+  delete chan;
+  return true;
+}
+
+bool
+Mux::SendNak(unsigned int aChanId, unsigned int aSeq) {
+  auto nak = std::make_unique<MuxPacket>(sizeof(MuxPacket), aChanId, MuxPacket::MuxCmd::Nak);
+  // The sequence number of a nak packet are the same as the packet
+  // received and triggering the nak packet.
+  nak->mSeq = aSeq;
+  auto ok = DoSend(nak.get());
+  return ok;
+}
+
+bool
+Mux::DoSend(MuxPacket* aPacket) {
+  auto r = mTransport->Write(aPacket, aPacket->mSize);
+  auto ok = r == aPacket->mSize;
+  return ok;
+}
+
+bool
+Mux::ReceiveRaw(char* aData, int aSize) {
+  auto src = aData;
+  auto remain = aSize;
+  while (remain) {
+    // Full the input buffer.
+    auto copy = min(remain, MuxPacket::MaxPacketSize - mInputBufSize);
+    if (mInputBufSize == 0 && remain >= sizeof(MuxPacket)) {
+      // The whole packet header is in |aData|.
+      auto packet = reinterpret_cast<MuxPacket*>(src);
+      copy = min(copy, packet->mSize);
+    } else if (mInputBufSize >= sizeof(MuxPacket)) {
+      // The whole packet header is in |mInputBuf|.
+      auto packet = reinterpret_cast<MuxPacket*>(mInputBuf);
+      copy = min((int)copy, max(0, (int)packet->mSize - (int)mInputBufSize));
+    }
+
+    if (copy > 0) {
+      memcpy(mInputBuf + mInputBufSize, src, copy);
+    }
+
+    remain -= copy;
+    mInputBufSize += copy;
+    src += copy;
+
+    if (mInputBufSize < sizeof(MuxPacket)) {
+      // Content is smaller than smallest packets.
+      break;
+    }
+
+    auto packet = reinterpret_cast<MuxPacket*>(mInputBuf);
+    if (packet->mSize > MuxPacket::MaxPacketSize) {
+      // The packet size is too big.
+      return false;
+    }
+    if (packet->mSize < mInputBufSize) {
+      // Content is not enough.
+      break;
+    }
+
+    HandleIncomingPacket(packet);
+
+    // Move the content in the input buffer.
+    auto rsz = mInputBufSize - packet->mSize;
+    if (rsz > 0) {
+      memmove(mInputBuf,
+              mInputBuf + packet->mSize,
+              rsz);
+    }
+    mInputBufSize = rsz;
+  }
+  return remain == 0;
+}
+
+
+#ifdef TEST
+
+#include <list>
+#include <functional>
+
+class Mock : public ChannelListener, public Transport {
+public:
+  struct ResourceInfo {
+    int mSize;
+    PacketListener* mHandler;
+    unsigned int mSeq;
+    unsigned int mChanId;
+    unsigned int mErr;
+    bool mFromPeer;
+
+    ResourceInfo(int aSize, PacketListener* aHandler)
+      : mSize(aSize)
+      , mHandler(aHandler) {}
+    ResourceInfo(ResourceInfo&& aOther)
+      : mSize(aOther.mSize)
+      , mHandler(aOther.mHandler) {
+      aOther.mHandler = nullptr;
+    }
+
+    ~ResourceInfo() {
+      if (mHandler) {
+        delete mHandler;
+      }
+    }
+  };
+
+  void SetPeer(Mock* aPeer) {
+    mPeer = aPeer;
+  }
+  void SetMux(Mux *aMux) {
+    mMux = aMux;
+  }
+
+  virtual void OnChannelOpened(unsigned int aSeq,
+                               unsigned int aErr,
+                               Channel* aChannel,
+                               const char* aPath,
+                               bool aFromPeer) override;
+  virtual int OnCheckDataSize(const char* aPath) override;
+
+  virtual int Write(void *aData, int aSize) override;
+
+  void Dispatch();
+
+  void AddResource(int aId, int aSize, PacketListener* aHandler) {
+    mResources.emplace(aId, std::forward<ResourceInfo>(ResourceInfo(aSize, aHandler)));
+  }
+  ResourceInfo* GetResource(int aId) {
+    auto itr = mResources.find(aId);
+    if (itr == mResources.end()) {
+      return nullptr;
+    }
+    return &itr->second;
+  }
+
+private:
+  struct Frame {
+    unsigned int mSize;
+    char mData[];
+
+    Frame(unsigned int aSize) : mSize(aSize) {}
+
+    static Frame* Create(unsigned int aSize) {
+      auto buf = new char[sizeof(Frame) + aSize];
+      return new(buf) Frame(aSize);
+    }
+  };
+
+  Mock* mPeer;
+  Mux* mMux;
+  std::list<Frame*> mIncomings;
+  std::map<int, ResourceInfo> mResources;
+};
+
+void
+Mock::OnChannelOpened(unsigned int aSeq,
+                      unsigned int aErr,
+                      Channel* aChannel,
+                      const char* aPath,
+                      bool aFromPeer) {
+  auto id = atoi(aPath);
+  auto resource = GetResource(id);
+  if (resource) {
+    resource->mErr = aErr;
+    resource->mFromPeer = aFromPeer;
+    if (aErr == 0) {
+      aChannel->SetListener(resource->mHandler);
+      resource->mSeq = aSeq;
+      resource->mChanId = aChannel->GetChannelId();
+    }
+  } else {
+    abort();
+  }
+}
+
+int
+Mock::OnCheckDataSize(const char* aPath) {
+  printf("Mock::OnCheckDataSize path %s\n", aPath);
+  auto id = atoi(aPath);
+  auto resource = GetResource(id);
+  if (resource) {
+    printf("  size %d\n", resource->mSize);
+    return resource->mSize;
+  }
+  return -1;
+}
+
+int
+Mock::Write(void *aData, int aSize) {
+  printf("Mock::Write size %d\n", aSize);
+  auto frame = Frame::Create(aSize);
+  memcpy(frame->mData, aData, aSize);
+  assert(mPeer != nullptr);
+  mPeer->mIncomings.push_back(frame);
+  return aSize;
+}
+
+void
+Mock::Dispatch() {
+  assert(mMux != nullptr);
+  for (auto itr = mIncomings.begin(); itr != mIncomings.end(); ++itr) {
+    printf("Dispatch packet size %d\n", (*itr)->mSize);
+    mMux->ReceiveRaw((*itr)->mData, (*itr)->mSize);
+    delete *itr;
+  }
+  mIncomings.clear();
+}
+
+class MockPacketListener : public PacketListener {
+public:
+  typedef std::function<void (const MuxPacket*)> ReceiverType;
+
+  MockPacketListener(ReceiverType&& aReceiver) : mReceiver(std::forward<ReceiverType>(aReceiver)) {}
+
+  virtual void OnReceive(Channel* aChannel, const MuxPacket* aPacket) override {
+    mReceiver(aPacket);
+  }
+  virtual void OnClose(Channel* aChannel) override {
+    mClosed = true;
+  }
+  virtual void OnError(Channel* aChannel, PacketListener::Error aErr) override {
+    mError = true;
+  }
+
+  ReceiverType mReceiver;
+  bool mClosed;
+  bool mError;
+};
+
+void
+test_open() {
+  int result1 = 0;
+  int result2 = 0;
+  auto listener1 = new MockPacketListener(MockPacketListener::ReceiverType([&](const MuxPacket* aPacket) {
+        assert(aPacket->mCmd == MuxPacket::MuxCmd::Data);
+        auto data = reinterpret_cast<const DataPacket*>(aPacket);
+        for (int i = 0; i < data->mPayloadSize; i++) {
+          result1 += data->mPayload[i];
+        }
+      }));
+  auto listener2 = new MockPacketListener(MockPacketListener::ReceiverType([&](const MuxPacket* aPacket) {
+        assert(aPacket->mCmd == MuxPacket::MuxCmd::Data);
+        auto data = reinterpret_cast<const DataPacket*>(aPacket);
+        for (int i = 0; i < data->mPayloadSize; i++) {
+          result2 += data->mPayload[i];
+        }
+      }));
+  Mock mock1;
+  Mock mock2;
+  mock1.SetPeer(&mock2);
+  mock2.SetPeer(&mock1);
+  mock1.AddResource(2, 8181, listener1);
+  auto resource1 = mock1.GetResource(2);
+  mock2.AddResource(2, 18181, listener2);
+  auto resource2 = mock2.GetResource(2);
+  Mux mux1(Mux::MuxSide::SideServer);
+  Mux mux2(Mux::MuxSide::SideClient);
+  mux1.SetChanListener(&mock1);
+  mux1.SetTransport(&mock1);
+  mux2.SetChanListener(&mock2);
+  mux2.SetTransport(&mock2);
+  mock1.SetMux(&mux1);
+  mock2.SetMux(&mux2);
+
+  auto seq_open = mux1.Open("2");
+  mock2.Dispatch();
+  assert(!resource2->mFromPeer);
+  printf("seq %d %d\n", seq_open, resource2->mSeq);
+  assert(resource2->mSeq == seq_open);
+  assert(resource2->mErr == 0);
+  mock1.Dispatch();
+  assert(resource1->mFromPeer);
+  assert(resource1->mSeq == seq_open);
+  assert(resource1->mErr == 0);
+
+  assert(resource1->mChanId == resource2->mChanId);
+
+  auto chan_id = resource1->mChanId;
+  auto chan1 = mux1.GetChannel(chan_id);
+  assert(!listener1->mClosed);
+  assert(!listener2->mClosed);
+  chan1->Close();
+  assert(listener1->mClosed);
+  assert(!listener2->mClosed);
+  mock2.Dispatch();
+  assert(listener2->mClosed);
+}
+
+int
+main(int argc, const char* argv[]) {
+  test_open();
+}
+
+#endif
---
+++ mux/mux.h
@@ -0,0 +1,329 @@
+/* -*- Mode: C++; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ * vim: set ts=8 sts=2 et sw=2 tw=80:
+ */
+#ifndef __MUX_H_
+#define __MUX_H_
+
+#include <string.h>
+#include <assert.h>
+#include <map>
+#include <new>
+
+#define MUX_CONTROL_CHANNEL 0
+
+#define SERVER_SIDE_MASK 0x0
+#define CLIENT_SIDE_MASK 0x1
+#define CHANNEL_MASK 0x1
+
+struct MuxPacket {
+  enum MuxCmd {
+    Hello,
+    Ack,
+    Nak,
+    // Channel control
+    OpenChan,
+    OpenChanReply,
+    CloseChan,
+    // Data payload
+    Data,
+
+    LastStdCmd
+  };
+
+  static constexpr unsigned int MaxPacketSize = 4096;
+
+  unsigned int mSize;
+  unsigned int mChan;
+  unsigned int mSeq;            // Will be filled by |Channel::Send()|
+                                // for most cases.
+  MuxCmd mCmd;
+
+  MuxPacket(unsigned int aSize, unsigned int aChan, MuxCmd aCmd)
+    : mSize(aSize)
+    , mChan(aChan)
+    , mCmd(aCmd) {
+    assert(aSize <= MaxPacketSize);
+  }
+};
+
+struct OpenChanPacket : public MuxPacket {
+  unsigned int mPathLen;
+  char mPath[];
+
+  static constexpr int MaxPathLen =
+    MaxPacketSize - sizeof(MuxPacket) - sizeof(mPathLen);
+
+  OpenChanPacket(unsigned int aPathLen)
+    : MuxPacket(SizeOfPathLen(aPathLen),
+                0,
+                MuxCmd::OpenChan) {
+  }
+
+  static unsigned int SizeOfPathLen(unsigned int aLen) {
+    return sizeof(OpenChanPacket) + aLen + 1;
+  }
+
+  static OpenChanPacket* Create(const char* aPath) {
+    auto pathlen = strlen(aPath);
+    auto size = SizeOfPathLen(pathlen);
+    auto buf = new char[size];
+    assert(strlen(aPath) <= MaxPathLen);
+
+    OpenChanPacket* p = new(buf) OpenChanPacket(pathlen);
+    p->mPathLen = strlen(aPath);
+    strcpy(p->mPath, aPath);
+    return p;
+  }
+};
+
+struct OpenChanReplyPacket : public MuxPacket {
+  enum Error {
+    Ok = 0,
+    UnknownError,
+    PermissionDeny
+  };
+  unsigned int mReqSeq;
+  unsigned int mErrno;
+  unsigned int mDataSize;
+  unsigned int mDataChan;
+
+  OpenChanReplyPacket(unsigned int aReqSeq,
+                      unsigned int aErr,
+                      unsigned int aDataSize,
+                      unsigned int aChan)
+    : MuxPacket(sizeof(OpenChanReplyPacket), 0, MuxCmd::OpenChanReply)
+    , mReqSeq(aReqSeq)
+    , mErrno(aErr)
+    , mDataSize(aDataSize)
+    , mDataChan(aChan)
+  {}
+};
+
+struct DataPacket : public MuxPacket {
+  unsigned int mPayloadSize;
+  char mPayload[];
+
+  static constexpr unsigned int MaxPayloadSize =
+    MaxPacketSize - sizeof(MuxPacket) - sizeof(mPayloadSize);
+
+  DataPacket(unsigned int aPayloadSIze, unsigned int aChan)
+    : MuxPacket(sizeof(DataPacket) + aPayloadSIze, aChan, MuxCmd::Data)
+    , mPayloadSize(aPayloadSIze)
+  {
+    assert(aPayloadSIze <= MaxPayloadSize);
+  }
+
+  static DataPacket* Create(unsigned int aPayloadSize, unsigned int aChan) {
+    auto sz = sizeof(DataPacket) + aPayloadSize;
+    auto buf = new char[sz];
+    return new(buf) DataPacket(aPayloadSize, aChan);
+  }
+};
+
+class Channel;
+
+class PacketListener {
+public:
+  enum Error {
+    Ok = 0,
+    UnknownError,
+    ProtocolError,
+    NetworkError,
+  };
+  virtual void OnReceive(Channel* aChannel, const MuxPacket* aPacket) = 0;
+  virtual void OnClose(Channel* aChannel) = 0;
+  virtual void OnError(Channel* aChannel, Error aErr) = 0;
+};
+
+class Mux;
+class StreamChannel;
+
+class Channel {
+public:
+  typedef PacketListener::Error Error;
+
+  enum Type {
+    TypePacketChannel,
+    TypeStreamChannel
+  };
+
+  static constexpr unsigned int first_seq = 1;
+
+  Channel(unsigned int aId, unsigned int aSize, Mux *aMux, Type aType = TypePacketChannel)
+    : mChanId(aId)
+    , mSize(aSize)
+    , mMux(aMux)
+    , mType(aType)
+    , mSeq(first_seq)
+    , mSeqPeer(first_seq)
+    , mValid(true) {}
+
+  void SetListener(PacketListener* aListener) {
+    mListener = aListener;
+  }
+
+  unsigned int GetChannelId() {
+    return mChanId;
+  }
+
+  unsigned GetSize() { return mSize; }
+
+  void Close();
+
+  bool Send(MuxPacket* aPacket);
+
+  // Handle incoing packets from Mux
+  void HandleIncomingPacket(const MuxPacket* aPacket);
+
+  Type GetChannelType() { return mType; }
+
+  virtual StreamChannel* GetStream() { return nullptr; }
+
+  void OnClose() {
+    assert(mListener != nullptr);
+    mListener->OnClose(this);
+    mValid = false;
+  }
+
+  void OnError(Error aErr) {
+    assert(mListener != nullptr);
+    mListener->OnError(this, aErr);
+    mValid = false;
+  }
+
+  bool IsValid() { return mValid; }
+
+private:
+  unsigned int mChanId;
+  unsigned int mSize;
+
+  Mux* mMux;
+  Type mType;
+  unsigned int mSeq;
+  unsigned int mSeqPeer;
+  PacketListener* mListener;
+  bool mValid;
+};
+
+class StreamListener {
+public:
+  virtual void OnReceive(StreamChannel* aChannel, unsigned int aSize, const char *aData) = 0;
+  virtual void OnClose(StreamChannel* aChannel) = 0;
+  virtual void OnError(StreamChannel* aChannel, Channel::Error aErr) = 0;
+};
+
+class StreamChannel : private Channel, private PacketListener {
+public:
+  StreamChannel(unsigned int aId, Mux* aMux) : Channel(aId, 0, aMux, TypeStreamChannel) {
+    Channel::SetListener(this);
+  }
+
+  void SetListener(StreamListener *aListener) {
+    mListener = aListener;
+  }
+
+  void CLose() {
+    Channel::Close();
+  }
+
+  bool Write(unsigned int aSize, const char *aData);
+
+  virtual StreamChannel* GetStream() override { return this; }
+
+  Channel* GetChannel() {
+    return this;
+  }
+
+  bool IsValid() { return Channel::IsValid(); }
+
+protected:
+  StreamListener* mListener;
+
+private:
+  // Interface PacketListener
+  virtual void OnReceive(Channel* aChannel, const MuxPacket* aPacket) override;
+  virtual void OnClose(Channel* aChannel) override;
+  virtual void OnError(Channel* aChannel, PacketListener::Error aErr) override;
+};
+
+class ChannelListener {
+public:
+  virtual void OnChannelOpened(unsigned int aSeq,
+                               unsigned int aErr,
+                               Channel* aChannel,
+                               const char* aPath,
+                               bool aFromPeer) = 0;
+  virtual int OnCheckDataSize(const char* aPath) = 0;
+};
+
+
+class Transport {
+public:
+  virtual int Write(void *aData, int aSize) = 0;
+};
+
+
+class Mux : private PacketListener {
+public:
+  enum MuxSide {
+    SideServer,
+    SideClient
+  };
+
+  Mux(MuxSide aSide);
+
+  void SetChanListener(ChannelListener* aListener) {
+    mChanListener = aListener;
+  }
+
+  void SetTransport(Transport* aTransport) {
+    mTransport = aTransport;
+  }
+
+  unsigned int Open(const char* aPath);
+
+  // Send packets for the instances of Channel.
+  bool DoSend(MuxPacket* aPacket);
+
+  // Receive raw bytes from the Transport.
+  bool ReceiveRaw(char *aData, int aSize);
+
+  // Channel ask Mux to close itself.
+  void ChannelAskClose(unsigned int aChanId);
+
+  Channel* GetChannel(unsigned int aChanId) {
+    if (aChanId == 0) {
+      return &mControlChan;
+    }
+    auto chanitr = mChannels.find(aChanId);
+    if (chanitr == mChannels.end()) {
+      return nullptr;
+    }
+    return chanitr->second;
+  }
+
+private:
+  // Handle incoming packets constructed by |ReceiveRaw()|.
+  void HandleIncomingPacket(const MuxPacket* aPacket);
+  bool DoCloseChannel(unsigned int aChanId);
+  bool DoErrorChannel(unsigned int aChanId, Channel::Error aErr);
+  bool SendNak(unsigned int aChanId, unsigned int aSeq);
+
+  // Interface PacketListener
+  virtual void OnReceive(Channel* aChannel, const MuxPacket* aPacket) override;
+  virtual void OnClose(Channel* aChannel) override;
+  virtual void OnError(Channel* aChannel, PacketListener::Error aErr) override;
+
+  MuxSide mSide;
+  Transport* mTransport;
+  Channel mControlChan;
+  ChannelListener *mChanListener;
+
+  unsigned int mNextChanId;
+  std::map<unsigned int, Channel*> mChannels;
+  std::map<unsigned int, MuxPacket*> mWaitingPackets;
+  int mInputBufSize;
+  char mInputBuf[MuxPacket::MaxPacketSize];
+};
+
+#endif
