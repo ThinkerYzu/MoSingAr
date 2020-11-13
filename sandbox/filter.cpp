fc4f5376a86af05c5e94bba158c791bfed8c584c
Thinker Lee <thinker@codemud.net>
2020-11-12 21:19:38 -0800

    Trap more syscalls.

--- sandbox/filter.cpp
+++ sandbox/filter.cpp
@@ -14,12 +14,18 @@
 static struct sock_filter filter[] = {
   BPF_STMT(BPF_LD | BPF_W | BPF_ABS,
            (offsetof(struct seccomp_data, nr))),
-  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_open, 1, 0),
-  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_openat, 0, 1),
-  BPF_STMT(BPF_RET | BPF_K,
-           SECCOMP_RET_TRAP),
-  BPF_STMT(BPF_RET | BPF_K,
-           SECCOMP_RET_ALLOW),
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_open, 10, 0),
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_openat, 9, 0),
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_access, 8, 0),
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_fstat, 7, 0),
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_lstat, 6, 0),
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_execve, 5, 0),
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_readlink, 4, 0),
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_stat, 3, 0),
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_unlink, 2, 0),
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_vfork, 1, 0),
+  BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
+  BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_TRAP),
 };
 
 struct sock_fprog sandbox_filter_prog = {
